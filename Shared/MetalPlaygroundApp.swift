a1cbb13060d7322260fe16893188daefa6f41e32
lizhen.craig <lizhen.craig@byetdance.com>
2021-05-16 15:56:44 +0800

    Initial Commit

---
+++ Shared/MetalPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalPlaygroundApp.swift
+//  Shared
+//
+//  Created by Craig on 2021/5/16.
+//
+
+import SwiftUI
+
+@main
+struct MetalPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
